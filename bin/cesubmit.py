57112afdc96a70d37a93567fbc20ae2d61b9e014
David Kleiven <david.kleiven@ntnu.no>
2017-09-20 10:00:15 +0200

    Setup scripts + some small changes

--- bin/cesubmit.py
+++ bin/cesubmit.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 import sys
 from dbsubmit.commandParser import CommandLineArgParser
-from dbsubmit.specificSubmitters import SimpleStatusFieldSubmit
+from dbsubmit.specificSubmitters import ASE
 
 def main( argv ):
     parser = CommandLineArgParser( argv )
-    submitter = SimpleStatusFieldSubmit( parser.arguments )
+    submitter =ASEClusterExpansionSubmit( parser.arguments )
     submitter.submit()
 
 if __name__ == "__main__":
