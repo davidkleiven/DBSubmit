3415095533ca2cb7b6767dcb0d1e828d2eb8bf4c
David Kleiven <davidkleiven446@gmail.com>
2018-01-20 17:27:11 +0100

    Removed tests.sh from the ignore file

---
+++ tests.sh
@@ -0,0 +1,7 @@
+# Test script for the DBSubmit module
+
+# Test the simplesubmit command
+python bin/simplesubmit.py  --dbname=example.db --nproc=2 --workdir=./ --dbtable=runs
+
+# Test the Cluster Expansion submit command
+python bin/simplesubmit.py --dbname=example.db --nproc=2 --workdir=./
