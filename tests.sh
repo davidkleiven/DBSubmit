d146838fbb2be70dd3891321ff8206de5c88ba91
David Kleiven <davidkleiven446@gmail.com>
2018-01-20 17:33:09 +0100

    Running tests for cesubmit

--- tests.sh
+++ tests.sh
@@ -4,4 +4,4 @@
 python bin/simplesubmit.py  --dbname=example.db --nproc=2 --workdir=./ --dbtable=runs
 
 # Test the Cluster Expansion submit command
-python bin/simplesubmit.py --dbname=example.db --nproc=2 --workdir=./
+python bin/cesubmit.py --dbname=example.db --nproc=2 --workdir=./
